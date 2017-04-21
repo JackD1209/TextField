2d7ac8f6fe1ea943791351cbbd5c9bcca5825bb8
JackD1209 <hoangdoan129.it@gmail.com>
2017-04-21 23:08:52 +0700

    2nd commit

---
+++ Pods/Target Support Files/Pods-TextFieldApp/Pods-TextFieldApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TextFieldApp : NSObject
+@end
+@implementation PodsDummy_Pods_TextFieldApp
+@end
--- TextFieldApp/FirstViewController.swift
+++ TextFieldApp/FirstViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class FirstViewController: UIViewController {
 
@@ -25,16 +26,27 @@ class FirstViewController: UIViewController {
     }
 
     @IBAction func btnClicked(_ sender: Any) {
-        checkLogin(user: username.text!, pass: password.text!, success:
-            {
-                self.performSegue(withIdentifier: "moveTo2ndScreen", sender: nil)
-            },
-            error:
-            {
-                let alert = UIAlertController(title: "Alert", message: "Username or password is not correct", preferredStyle:UIAlertControllerStyle.alert)
+//        checkLogin(user: username.text!, pass: password.text!, success:
+//            {
+//                self.performSegue(withIdentifier: "moveTo2ndScreen", sender: nil)
+//            },
+//            error:
+//            {
+//                let alert = UIAlertController(title: "Alert", message: "Username or password is not correct", preferredStyle:UIAlertControllerStyle.alert)
+//                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+//                self.present(alert, animated: true, completion: nil)
+//            })
+        
+        FIRAuth.auth()?.signIn(withEmail: username.text!, password: password.text!) { (user, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Alert", message: "Username or password is not correct",preferredStyle:UIAlertControllerStyle.alert)
                 alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
                 self.present(alert, animated: true, completion: nil)
-            })
+            }
+            else {
+                self.performSegue(withIdentifier: "moveTo2ndScreen", sender: nil)
+            }
+        }
 
     }
     
@@ -45,13 +57,13 @@ class FirstViewController: UIViewController {
         }
     }
     
-    func checkLogin(user: String, pass: String, success: () -> (), error: () -> ()) {
-        if user == "admin" && pass == "admin" {
-            success()
-        }
-        else {
-            error()
-        }
-    }
+//    func checkLogin(user: String, pass: String, success: () -> (), error: () -> ()) {
+//        if user == admin && pass = admin {
+//            success()
+//        }
+//        else {
+//            error()
+//        }
+//    }
     
 }
